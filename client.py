ced7b55614b8a07371ff771c586d84059d07b1dc
anitah12 <56592972+anitah12@users.noreply.github.com>
2019-10-21 17:46:21 +0300

    updated client.py

--- client.py
+++ client.py
@@ -1,19 +1,53 @@
 import asyncio
 
+
 async def tcp_echo_client():
+    commands_history=[] 
     reader, writer = await asyncio.open_connection(
         '127.0.0.1', 8888)
     message = ''
     while True:
         message = input('>')
+        commands_history.append(message)
+        if(message.startswith('commands')):
+            await commands(message, commands_history)
         writer.write(message.encode())
         data = await reader.read(100)
         print(f'Received: {data.decode()}')
     print('Close the connection')
     writer.close()
 
+async def commands(value, commands_history):
+    """Input availble commands with options for the user."""
+     
+    if value == 'commands':
+        """input availble commands"""
+        print("""
+             change_folder <name>
+             list
+             read_file <name>
+             write_file <name>
+             input
+             create_folder <name>
+             register <username>  <password> <priviledges>
+             login <username> <password>
+             delete <username> <password>
+             quit
+             """)
+    elif value=='commands issued':
+         """Input issued by the user"""
+         for i in commands_history:
+             print(i)
+                
+    elif value == 'commands clear':
+         """Clear commands"""
+         commands_history==[]
+         print("All the commands are cleared")
+          
+    else:
+        """False input"""
+        print("Invalid input")
+          
 async def quit():
-    pass
-async def commands():
     pass
 asyncio.run(tcp_echo_client())
