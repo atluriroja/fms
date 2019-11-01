4734eb7d9e553ad7b6f65c1bed6c9c8634a606f9
anitah12 <uwinezanitah@gmail.com>
2019-11-01 15:41:08 +0200

    fns

--- src/filecommands.py
+++ src/filecommands.py
@@ -1,6 +1,8 @@
 import os
 import sys
-
+from path import Path
+import datetime
+import time
 
 class Commands():
 
@@ -55,3 +57,64 @@ class Commands():
             #     print(contents[0:100])
         except IOError:
             print('Error.' + files)
+
+
+
+
+
+
+
+root='E:\\Ass3\\fms\\client'
+def list(self, path):
+    
+    """check current working directory"""
+    cwd=os.getcwd()
+    """move to current working directory"""
+    cwd=Path(path)
+    """current working directory"""
+
+    directory = os.listdir(path)
+    """initiaalizing files and directories 
+    in current working directory"""
+    for d in directory:
+        folder_path=os.path.join(d)
+        print(d)
+
+    """initializing file size in 
+     the current working directory"""
+    for file in cwd.files():
+        """path for current working directory"""
+        folder_path=os.path.join(file)
+        """date & time of creating files 
+        current working directory"""
+        last_Mod=os.stat(file).st_ctime
+        """size of the files in the current directory"""
+        size=os.stat(file).st_size
+        print(folder_path)
+        print("Size:",size)
+        print(datetime.datetime.strptime(time.ctime(last_Mod),"%a %b %d %H:%M:%S %Y"))
+        return ""
+
+
+def write_file():
+    # asks user for filename
+    newfile = input("Insert 'newfile.txt' >>> ")
+    #clear_files=[]
+ 
+    # opens user inputted filename ".txt" and (w+) makes new and writes
+    with open(newfile, 'w') as f:
+        # asks for user input to enter into the file
+        user_input = input("user input >>> ")
+        # writes user input to the file and adds new line
+        if user_input==[]:
+            f.write("")
+            #return(clear_files)
+    
+        elif user_input!=[]:
+           f.write(user_input)
+        
+        f.write("\n")
+    return ""
+    
+            
+           
