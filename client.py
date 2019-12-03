582f5da75e1cdbe60046be19e5e0725d3b3f8cd6
rojasameer <roja.mandava@gmail.com>
2019-12-03 10:52:03 +0100

    Refactored the code remove unnecessary imports and Updated the docstrings

--- client.py
+++ client.py
@@ -1,14 +1,18 @@
 import asyncio
-import os
-from path import Path
-import datetime
-import time
 
 commands_history = []
+
+"""
+   A TCP client function, establish the connection with the server to handle the user 
+   commands and closes the connection on quit.   
+"""
 async def tcp_echo_client():
     
     reader, writer = await asyncio.open_connection(
         '127.0.0.1', 8000)
+    print(reader)
+    assert reader is not None, "Server doesn't return a Asyncio StreamReader object "
+    assert writer is not None, "Server doesnt rerurn a Asyncio Streamwriter object"
     message = ''
     while True:
         message = input('>>')
@@ -27,12 +31,24 @@ async def tcp_echo_client():
     print('Close the connection')
     writer.close()
 
+"""
+    Checks if the user inputs valid command arguments/value.
 
+    If invalid argument, error message is displayed.
+    if no arguments, displays all the available commands
+    if the argument is 'issued', displays all the list of commands excuted.
+    if the arguments is 'clear', clears all the previous history of commands
+        
+    Parameters:
+        value : string
+            A value is a 'commands' command as a string with or without arguments.
+        commands_history : list
+            A commands_history is a list contains all the commands that user has sent.
+"""
 async def commands(value, commands_history):
-    """Input availble commands with options for the user."""
 
     if value == 'commands':
-        """input availble commands"""
+        """Print all the available commands"""
         print("""
              Available Commands:
              change_folder <name>
@@ -41,13 +57,13 @@ async def commands(value, commands_history):
              write_file <name>
              input
              create_folder <name>
-             register <username>  <password> <priviledges>
+             register <username> <password> <priviledges>
              login <username> <password>
              delete <username> <password>
              quit
              """)
     elif value == 'commands issued':
-        """Input issued by the user"""
+        """commands issued by the user"""
         for i in commands_history:
             print(i)
 
